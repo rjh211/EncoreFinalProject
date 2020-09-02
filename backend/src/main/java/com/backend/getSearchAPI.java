01b73fbc8d71dffb8bf5a89e6279c039f2a9d349
rjh211 <rjh211@naver.com>
2020-09-02 18:01:54 +0900

    add 백종원 게임

---
+++ backend/src/main/java/com/backend/getSearchAPI.java
@@ -0,0 +1,122 @@
+package com.backend;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+@RestController
+@CrossOrigin(origins = "*") // @CrossOrigin(origins = "http://localhost:8080")
+public class getSearchAPI{
+    private String clientId = "lYK2DHzFa5rO9aVz3vF9";
+    private String clientSecert = "WQUbtyQgTj";
+    @GetMapping(value="/callSearchAPI")
+    public boolean getSearch(@RequestParam("word") String word, @RequestParam("count") int count){
+        String text = null;
+        try {
+            text = URLEncoder.encode(word, "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException("검색어 인코딩 실패",e);
+        }
+
+        String apiUrl = "https://openapi.naver.com/v1/search/blog?query=" + text + "&display="+ count;
+
+        Map<String,String> requestHeaders = new HashMap();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecert);
+        String responseBody = get(apiUrl, requestHeaders);
+
+        JsonParser jsonParser = JsonParserFactory.getJsonParser();
+        Map<String, Object> body = jsonParser.parseMap(responseBody);
+        System.out.println(body);
+        if(body.get("items").toString().contains("백종원")){
+            return true;
+        }
+        return false;
+    }
+
+    private static String get(String apiUrl, Map<String, String> requestHeaders){
+        HttpURLConnection con = connect(apiUrl);
+        try {
+            con.setRequestMethod("GET");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                return readBody(con.getInputStream());
+            } else { // 에러 발생
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+}
---
+++ frontend/src/components/Game3/Game.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      음식 명 : <input type="text" v-model="word"><br/>
+      검색 개수 : <input type="text" v-model="searchCount"><br/>
+      <button @click="callSearchApi">검색</button><br/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            word:'김치찌개',
+            searchCount:5
+        }
+    },
+    methods:{
+        callSearchApi(){
+            axios.get('http://localhost:8000/callSearchAPI?word='+this.word + '&count=' + this.searchCount)
+            .then((res)=>{
+                if(res.data) alert('꽝 입니다.');
+                this.word = '';
+                this.searchCount = '';
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
